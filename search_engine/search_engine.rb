5b916adabbf635c978d64bb361cbb2de1ac44482
Wendy Hsu <wndyhsu@gmail.com>
2014-04-30 23:53:19 -0500

    adding title into search results

--- search_engine/search_engine.rb
+++ search_engine/search_engine.rb
@@ -5,11 +5,12 @@ require_relative 'lib/reader.rb'
 
 class SearchEngine
   include Reader
-  attr_accessor :books, :density_hash
+  attr_accessor :books, :density_hash, :result_array
   # call read_books and set up your data structures to make it easier when you call search
   def initialize (folder)
     @books = read_books(folder)
     @density_hash = {}
+    @result_array = []
 
   end
 
@@ -20,15 +21,22 @@ class SearchEngine
 
 
   def search(query) # Implement this search, return an array of book names in order of relatedness
-    # automatically returns an array #keys
-    @books.each do |k,v|
-      density = v.count(query).to_f / v.length.to_f
-      @density_hash[k] = density
-    end
-    result = @density_hash.sort_by{|k,v| v}.reverse.map(&:first)
-    # the result is an array of all title sorted
-  end
-end
+     @books.each do |k,v|
+       if k.include?(query)
+         @result_array.push(k.to_s)
+       end
+       @result_array
+     end
+     @books.each do |k,v|
+       density = v.count(query).to_f/ v.length.to_f
+       @density_hash[k] = density
 
+     end
+      result = @density_hash.sort_by{|k, v| v}.reverse.map(&:first)
+      result.each do |titles|
+       @result_array.push(titles)
+      end
+    end
+end
 
 
